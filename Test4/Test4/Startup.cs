031b6fcd4c732ad2e5c62a0f8d3987d43bf4fc60
JakobAV <Jakob.Persson@avensia.com>
2017-02-08 14:49:23 +0100

    asd

---
+++ Test4/Test4/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Test4.Startup))]
+namespace Test4
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
